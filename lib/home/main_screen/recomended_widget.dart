fd23de4131dcf48a3eb43509a152a8d901b681d7
asalah <90047716+asalahhh@users.noreply.github.com>
2022-09-09 00:18:37 +0200

    added main screen

---
+++ lib/home/main_screen/recomended_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class RecomendedWidget extends StatefulWidget {
+  @override
+  State<RecomendedWidget> createState() => _RecomendedWidgetState();
+}
+
+class _RecomendedWidgetState extends State<RecomendedWidget> {
+  bool isMarked=false;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Image.asset('assets/images/doraFilm.png',width: 97,height: 128,),
+        Positioned(
+            left: size.width*0.02,
+            child: InkWell(
+                onTap: (){
+
+                  setState((){
+                    isMarked=!isMarked;
+                  });
+                },
+                child: Image.asset(isMarked?'assets/images/bookmark_marked.png':'assets/images/bookmark.png',width: 20,))),
+
+      ],
+    );
+  }
+
+  List<String>boockMark=[
+
+  ];
+}
