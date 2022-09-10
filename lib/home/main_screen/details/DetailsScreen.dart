19e5fec10d1ece8badb2c85fce63e9b189265d93
EsraaHass <israa.hassan517@gmail.com>
2022-09-10 11:55:58 -0700

    Make UI for Details Screen.

--- lib/home/main_screen/details/DetailsScreen.dart
+++ lib/home/main_screen/details/DetailsScreen.dart
@@ -69,6 +69,9 @@ class DetailsScreen extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                         color: Colors.white),
                   ),
+                  SizedBox(
+                    height: 15,
+                  ),
                   Expanded(
                     child: ListView.separated(
                       scrollDirection: Axis.horizontal,
