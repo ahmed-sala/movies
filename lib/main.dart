ef2e135e75b1ab2fe2da2a07e4ce521d0050878b
EsraaHass <israa.hassan517@gmail.com>
2022-09-11 08:55:32 -0700

    finish api for top side section.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Movies App',
+      title: 'Movies ',
       routes: {
         HomeScreen.routeName: (_) => HomeScreen(),
         DetailsScreen.routeName: (_) => DetailsScreen(),
